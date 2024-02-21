ac1c25bb46de562265abb5d7b139974472602eea
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-21 23:19:38 +0500

    added jsonPretty to the project

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import JSONPretty from "react-json-pretty";
 
 const Response = () => {
   const request = useSelector((state) =>
@@ -7,14 +8,32 @@ const Response = () => {
   );
 
   return (
-    <div className=" border-t h-72 mt-10 p-2 border overflow-y-auto ">
+    <div className="border-t w-[1200px] h-96 mt-10 p-2 border overflow-y-auto">
       {request?.response?.data ? (
         <>
           <p>Vaqti: {request?.response?.differenceTime} </p>
-          <env> {request?.response?.data} </env>
+          <JSONPretty
+            mainStyle="background: #fff; color: #000"
+            keyStyle="color: rgb(163, 21, 21)"
+            valueStyle="color: rgb(9, 134, 88)"
+            stringStyle="color: rgb(4, 81, 161)"
+            booleanStyle="color: rgb(4, 81, 161); font-weight: bold"
+            id="json-pretty"
+            data={request?.response?.data}
+          ></JSONPretty>
         </>
       ) : (
-        <p>Response</p>
+        <div className="absolute w-[1200px] h-96">
+          <p>Response</p>
+          <div className="relative top-1/3 left-1/3 ">
+            <img
+              className="mb-3"
+              src="https://postman.com/_aether-assets/illustrations/dark/illustration-hit-send.svg"
+              alt="Postman image"
+            />
+            <p className="-ml-8">Click Send to get a response</p>
+          </div>
+        </div>
       )}
     </div>
   );
